529037fbdbba6b17b1e904c1144fb0a14c121e4a
Sathish Vinayk <sathishvinayk@gmail.com>
2017-05-24 13:56:58 +0530

    Fixed_cannot_find_namespace_google

---
+++ readme.md
@@ -0,0 +1,11 @@
+//So far Cannot find namespace and Cannot find name was error.
+//Updated TS version to 2.3.3 and npm to 4.6.1
+Tried created Declaration.d.ts
+
+
+//Working
+Install googlemaps instead of google-maps for ionic>=version 2
+npm install --save @types/googlemaps
+
+//Import byv using below line
+import {} from '@types/googlemaps';
--- src/pages/park-map/park-map.ts
+++ src/pages/park-map/park-map.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Platform, NavController, NavParams } from 'ionic-angular';
+import {} from '@types/googlemaps';
 
-/*
-  Generated class for the ParkMap page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-park-map',
   templateUrl: 'park-map.html'
 })
 export class ParkMapPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ParkMapPage');
+  map: google.maps.Map;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
+    this.map=null;
+    this.platform.ready().then(()=>{
+      this.initializeMap();
+    });
   }
-
+  initializeMap(){
+    let minZoomLevel=3;
+    this.map=new google.maps.Map(document.getElementById('map_canvas'),{
+      zoom:minZoomLevel,
+      center: new google.maps.LatLng(39.833, -98.583),
+      mapTypeControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+  };
 }
