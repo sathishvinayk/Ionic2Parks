3b63571063ac175246481d2e6f6ecb61bc847e5b
Sathishvinayk <sathishvinayk@gmail.com>
2017-05-25 19:09:39 +0530

    Markers with google map fixed

---
+++ src/interfaces/park.ts
@@ -0,0 +1,11 @@
+interface Park {
+  id: number;
+  name: string;
+  createDate: string;
+  lat: number;
+  long: number;
+  distance: number;
+  image: string;
+  state: string;
+  data: string;
+}
--- src/pages/park-map/park-map.ts
+++ src/pages/park-map/park-map.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { Platform, NavController, NavParams } from 'ionic-angular';
 import {} from '@types/googlemaps';
+import { ParkData } from "../../providers/park-data";
 
 @Component({
   selector: 'page-park-map',
   templateUrl: 'park-map.html'
 })
+
 export class ParkMapPage {
+  parks: Array<Park>=[];
   map: google.maps.Map;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public platform: Platform,
+              public parkData:ParkData) {
     this.map=null;
     this.platform.ready().then(()=>{
       this.ionViewDidLoad();
@@ -21,6 +27,7 @@ export class ParkMapPage {
   }
   initializeMap(){
     let minZoomLevel=3;
+    let image='assets/img/nps_arrowhead.png';
     this.map=new google.maps.Map(document.getElementById('map_canvas'),{
       zoom:minZoomLevel,
       center: new google.maps.LatLng(39.833, -98.583),
@@ -28,5 +35,17 @@ export class ParkMapPage {
       streetViewControl: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+    this.parkData.getParks().then(theResult=>{
+      this.parks=theResult;
+
+      for(let thePark of this.parks){
+        let parkPos:google.maps.LatLng=
+          new google.maps.LatLng(thePark.lat, thePark.long);
+        let parkMarker:google.maps.Marker=new google.maps.Marker();
+        parkMarker.setPosition(parkPos);
+        parkMarker.setMap(this.map);
+        parkMarker.setIcon(image);
+      }
+    })
   };
 }
