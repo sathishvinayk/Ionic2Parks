3c0f2822ecf371155e0bcdec29c733d937179047
Sathishvinayk <sathishvinayk@gmail.com>
2017-05-28 19:49:13 +0530

    Copied Custom marker codes from BranchC

--- src/pages/park-map/park-map.ts
+++ src/pages/park-map/park-map.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { Platform, NavController, NavParams } from 'ionic-angular';
 import {} from '@types/googlemaps';
 import { Park } from "../../interfaces/park";
-import { ParkData }from "../../providers/park-data";
+import { ParkData } from "../../providers/park-data";
+import { ParkDetails }from "../park-details/park-details";
+import { CustomMapMarker }from "./custom-marker";
 
 @Component({
   selector: 'page-park-map',
@@ -15,23 +17,20 @@ export class ParkMapPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public platform: Platform,
-              public parkData: ParkData) {
+              public parkData:ParkData) {
     this.map=null;
     this.platform.ready().then(()=>{
-      //Call the ionViewDidLoad() here.
       this.ionViewDidLoad();
     });
   }
   ionViewDidLoad(){
-    //set timeout so that Dom loads
     setTimeout(()=>{
-      //Call the initializeMap here
       this.initializeMap();
     },500)
   }
   initializeMap(){
     let minZoomLevel=3;
-    let image="assets/img/nps_arrowhead.png";
+    let image='assets/img/nps_arrowhead.png';
     this.map=new google.maps.Map(document.getElementById('map_canvas'),{
       zoom:minZoomLevel,
       center: new google.maps.LatLng(39.833, -98.583),
@@ -40,16 +39,22 @@ export class ParkMapPage {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     this.parkData.getParks().then(theResult=>{
-      console.log(typeof(ParkData));
       this.parks=theResult;
+
       for(let thePark of this.parks){
         let parkPos:google.maps.LatLng=
           new google.maps.LatLng(thePark.lat, thePark.long);
-        let parkMarker:google.maps.Marker=new google.maps.Marker();
+        let parkMarker:google.maps.Marker=new CustomMapMarker(thePark);
         parkMarker.setPosition(parkPos);
         parkMarker.setMap(this.map);
         parkMarker.setIcon(image);
+        google.maps.event.addListener(parkMarker, 'click', ()=>{
+          let selectedMarker: any = parkMarker;
+          this.navCtrl.push(ParkDetails,{
+            parkData: selectedMarker.parkData
+          });
+        })
       }
-    });
-  }
-};
+    })
+  };
+}
